59be290b00502800dfef71db11b4518b1322cb59
hzj5016 <hzj5016@github.com>
2018-12-26 18:38:54 +0800

    add 首页 swiper

--- src/main.js
+++ src/main.js
@@ -1,18 +1,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-// 解决手机不同的默认样式css库
 import 'styles/reset.css'
-// 解决手机一边框问题css库
-
-// 配置了webpack,把styles变成了src/assets/styles
 import 'styles/border.css'
 import 'styles/iconfont.css'
 import fastClick from 'fastclick'
-
-// 移动端点击事件300ms延迟,绑定到body上
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 fastClick.attach(document.body)
 Vue.config.productionTip = false
+
+Vue.use(VueAwesomeSwiper)
 
 let vm = new Vue({
   el: '#app',
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,54 @@
+<template>
+  <!-- 绑定了一个变量swiperOption -->
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+    <!-- slides -->
+    <swiper-slide v-for="item in swiperList" :key="item.id">
+      <img class="swiper-img" :src="item.imgUrl">
+    </swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination" slot="pagination"></div>
+  </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination'
+      },
+      swiperList: [
+        {
+          id: '001',
+          // 从本地加载图片需要用require引入
+          imgUrl: require(`@/assets/img/home/1.jpg`)
+        },
+        {
+          id: '002',
+          imgUrl: require(`@/assets/img/home/2.jpg`)
+        },
+        {
+          id: '003',
+          imgUrl: require(`@/assets/img/home/3.jpg`)
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+  background: #fff
+.wrapper
+  background: #eee
+  width: 100%
+  height: 0
+  padding-bottom: 30.6%
+  overflow: hidden
+  .swiper-img
+    width: 100%
+</style>

b7e1bcaa9abd868a8e414822ef2f1e50fd52f7e4
hzj5016 <hzj5016@github.com>
2018-12-26 18:46:43 +0800

    首页轮播 --> 循环轮播

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -18,7 +18,8 @@ export default {
   data () {
     return {
       swiperOption: {
-        pagination: '.swiper-pagination'
+        pagination: '.swiper-pagination',
+        loop: true
       },
       swiperList: [
         {

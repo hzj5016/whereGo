052e6e236e669ac745f4da910974cd4978f22713
hzj5016 <hzj5016@github.com>
2019-01-09 17:00:02 +0800

    优化Alphabet

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -21,8 +21,15 @@ export default {
   },
   data () {
     return {
-      touchState: false
+      touchState: false,
+      startY: null,
+      timer: null
     }
+  },
+  updated () {
+    // startY是A元素距离可视区域(已经绝对定位)最顶层的元素的高度,offsetTop是Vue提供的api
+    // startY的高度为75px
+    this.startY = this.$refs['A'][0].offsetTop
   },
   computed: {
     letters () {
@@ -41,15 +48,22 @@ export default {
     handleTouchStart () {
       this.touchState = true
     },
+    // 字母列表联动
     handleTouchMove (e) {
       if (this.touchState) {
-        // startY是A元素距离可视区域(已经绝对定位)最顶层的元素的高度,offsetTop是Vue提供的api
-        // startY的高度为75px
-        const startY = this.$refs['A'][0].offsetTop
-        // 这个clientY是距离最顶部的高度,相对窗口而言,和offsetTop有时候不同
-        const touchY = e.touches[0].clientY - 78.5
-        const distance = touchY - startY
-        console.log(touchY)
+        // 函数节流
+        if (this.timer) {
+          clearTimeout(this.timer)
+        }
+        this.timer = setTimeout(() => {
+          // 这个clientY是距离最顶部的高度,相对窗口而言,和offsetTop有时候不同
+          const touchY = e.touches[0].clientY - 78.5
+          const distance = touchY - this.startY
+          const index = Math.floor(distance / 20)
+          if (index >= 0 && index < this.letters.length) {
+            this.$emit('change', this.letters[index])
+          }
+        }, 16)
       }
     },
     handleTouchEnd () {
